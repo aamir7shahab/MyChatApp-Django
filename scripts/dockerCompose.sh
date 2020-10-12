605f34a2e9191aa243c356d4211cb48b1e2ba39d
Aamir Shahab <aamir.shahab007@gmail.com>
2020-10-13 04:47:10 +0530

    Update dockerCompose.sh

--- scripts/dockerCompose.sh
+++ scripts/dockerCompose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 sudo docker-compose -f /MyChatApp-Django/docker-compose.yaml down
-imageId=`sudo docker images | awk '{if ($1 =="mychatapp-django_djangoapp" || $1 =="postgres" || $1 =="python" || $1 =="nginx") print $3}'`
+imageId=`sudo docker images | awk '{if ($1 =="mychatapp-django_djangoapp") print $3}'`
 sudo docker rmi $imageId
 sudo docker-compose -f /MyChatApp-Django/docker-compose.yaml up -d 
